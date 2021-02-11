97347bdf2d84024420d9185df0a3f548f2d063e1
Tyler Long <tyler4long@gmail.com>
2021-02-11 15:14:41 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import RingCentral from '@rc-ex/core';
+import fs from 'fs';
+import path from 'path';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER,
@@ -9,6 +11,16 @@ rc.token = {
 };
 
 (async () => {
-  const ext = await rc.restapi().account().extension().get();
-  console.log(JSON.stringify(ext, null, 2));
+  const callLog = await rc
+    .restapi()
+    .account()
+    .extension()
+    .callLog(process.env.RINGCENTRAL_CALL_LOG_ID)
+    .get();
+  console.log(JSON.stringify(callLog, null, 2));
+
+  const r = await rc.get<Buffer>(callLog.recording!.contentUri!, undefined, {
+    responseType: 'arraybuffer',
+  });
+  fs.writeFileSync(path.join(__dirname, 'recording.wav'), r.data);
 })();
